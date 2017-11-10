718df65e58fd61795497d4aec95371d58a10cfea
Pablo <pablonm89@gmail.com>
2017-11-10 13:52:06 -0300

    Finish JS/JQ, add some css styling, make the code prettier

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 createGrid(16)
+mouseHover()
 
 function createGrid(n) {
-    for (var rows = 0; rows < n; rows++) {
-        for (var columns = 0; columns < n; columns++) {
-            $(".grid-container").append("<div class='grid'></div>")
-        }
-    }
-    $(".grid").width(800/n)
-    $(".grid").height(800/n)
+	for (var rows = 0; rows < n; rows++) {
+		for (var columns = 0; columns < n; columns++) {
+			$('.grid-container').append('<div class="grid"></div>')
+		}
+	}
+	$('.grid').width(800/n)
+	$('.grid').height(800/n)
 }
 
 function deleteGrid() {
-$('.grid-container').empty()
+	$('.grid-container').empty()
 }
 
 function promptGrid() {
-	var nn = prompt('Select new grid number')
+	var nn = prompt('Select a number from 1 to 64')
 	deleteGrid()
-	createGrid(nn)
+	if (nn <= 64 && nn != 0) {
+		createGrid(nn)
+	} else {
+		alert('Please select a valid number')
+	}
+	mouseHover()
+}
+
+function mouseHover() {
+	$('.grid').on('mouseover', function() {
+		$(this).css('background-color', '#333')
+	})
 }
 
 $('.js-btn').click(_ => {
-promptGrid()
+	promptGrid()
 })
