2d82f6bd5f5e3d080889986e42db6b5f0601b01b
Pablo <pablonm89@gmail.com>
2017-12-14 17:34:10 -0300

    Added counter and round limit.

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,3 +1,8 @@
+let userPoints = 0
+let computerPoints = 0
+let ties = 0
+let rounds = 0
+
 // return Rock/Paper/Scissor at random
 function computerPlay() {
 	let choice = ['Rock', 'Paper', 'Scissors']
@@ -10,27 +15,55 @@ function playRound(computerSelection) {
 	let playerChoice = prompt('Choose Rock, Paper or Scissors.').toLowerCase()
 	let computerChoice = computerSelection.toLowerCase()
 
+	// if random input
 	if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') return alert('Please select Rock, Paper or Scissors.')
 
-	if (playerChoice == computerChoice) return $('.para').text('It\'s a tie!')
+	// if tie
+	if (playerChoice == computerChoice) {
+		$('.para').text('It\'s a tie!')
+		ties +=1
+		return $('.counter').text('You: ' + userPoints + ' | Computer: ' + computerPoints + ' | ties: ' + ties)
+	}
 	
 	let rockPaper = (playerChoice == 'rock' && computerChoice == 'paper')
 	let paperRock = (playerChoice == 'paper' && computerChoice == 'rock')
 	let scissorsPaper = (playerChoice == 'scissors' && computerChoice == 'paper')
 
+	// checks who wins each round
 	if (rockPaper || paperRock || scissorsPaper) {
-		return $('.para').text('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
+		$('.para').text('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
+		userPoints +=1
+		return $('.counter').text('You: ' + userPoints + ' | Computer: ' + computerPoints + ' | ties: ' + ties)
 	} else {
-		return $('.para').text('You lose! ' + computerChoice + ' beats ' + playerChoice)
+		$('.para').text('You lose! ' + computerChoice + ' beats ' + playerChoice)
+		computerPoints +=1
+		return $('.counter').text('You: ' + userPoints + ' | Computer: ' + computerPoints + ' | ties: ' + ties)
 	}
 }
 
 // play n rounds of the game
 function playGame() {
 	playRound(computerPlay())
+	rounds +=1
+
+	// check who's the winner
+	if (rounds == 5 && userPoints > computerPoints) {
+		$('.result').text('FINAL RESULT: You have beaten the computer. Congratulations!')
+	} else if (rounds == 5 && userPoints < computerPoints) {
+		$('.result').text('FINAL RESULT: The computer beat you. Shame.')
+	} else if (rounds == 5 && userPoints == computerPoints) {
+		$('.result').text('FINAL RESULT: Well, this is awkward.')
+	}
 }
 
 // jQuery for the button
 $('.js-btn').click(_ => {
+	if (rounds == 5) {
+		rounds = 0
+		userPoints = 0
+		computerPoints = 0
+		ties = 0
+		$('p').empty()
+	}
 	playGame()
 });
