5dc96306ff7f51c3532bd14ab9deabeb1409c8cf
Pablo <pablonm89@gmail.com>
2017-12-14 16:29:54 -0300

    Rock, Paper, Scissors game

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,4 +1,3 @@
-
 // return Rock/Paper/Scissor at random
 function computerPlay() {
 	let choice = ['Rock', 'Paper', 'Scissors']
@@ -6,32 +5,32 @@ function computerPlay() {
 	return choice[selectRandom]
 }
 
+// plays a round of the game
 function playRound(computerSelection) {
 	let playerChoice = prompt('Choose Rock, Paper or Scissors.').toLowerCase()
 	let computerChoice = computerSelection.toLowerCase()
 
-	if (playerChoice == computerChoice) {
-		return alert('It\'s a tie!')
-	}
+	if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') return alert('Please select Rock, Paper or Scissors.')
 
-	if (playerChoice == 'rock' && computerChoice == 'paper') {
-		return alert('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
+	if (playerChoice == computerChoice) return $('.para').text('It\'s a tie!')
+	
+	let rockPaper = (playerChoice == 'rock' && computerChoice == 'paper')
+	let paperRock = (playerChoice == 'paper' && computerChoice == 'rock')
+	let scissorsPaper = (playerChoice == 'scissors' && computerChoice == 'paper')
+
+	if (rockPaper || paperRock || scissorsPaper) {
+		return $('.para').text('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
 	} else {
-		return alert('You lose! ' + computerChoice + ' beats ' + playerChoice)
-	}
-	if (playerChoice == 'paper' && computerChoice == 'rock') {
-		return alert('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
-	} else {
-		return alert('You lose! ' + computerChoice + ' beats ' + playerChoice)
-	}
-	if (playerChoice == 'scissors' && computerChoice == 'paper') {
-		return alert('You win! ' + playerChoice + ' ' + 'beats' + ' ' + computerChoice)
-	} else {
-		return alert('You lose! ' + computerChoice + ' beats ' + playerChoice)
+		return $('.para').text('You lose! ' + computerChoice + ' beats ' + playerChoice)
 	}
 }
 
+// play n rounds of the game
 function playGame() {
 	playRound(computerPlay())
 }
 
+// jQuery for the button
+$('.js-btn').click(_ => {
+	playGame()
+});
