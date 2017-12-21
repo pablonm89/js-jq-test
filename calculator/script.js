de9b2eb2e83b4375763d21a8c006e0addefec2fd
Pablo <pablonm89@gmail.com>
2017-12-21 09:47:15 -0300

    Tweaks and fixes

--- calculator/script.js
+++ calculator/script.js
@@ -1,19 +1,58 @@
-function add(a, b) {
-	return a + b
-}
+//clears the equation
+$('.clear').click(_ => clear())
 
-function substract(a, b) {
-	return a - b
-}
-
-function multiply(a, b) {
-	return a * b
-}
-
-function divide(a, b) {
-	return a / b
-}
+const add = (a, b) => a + b
+const sub = (a, b) => a - b
+const mult = (a, b) => a * b
+const div = (a, b) => a / b
 
 function operate(operator, a, b) {
-return operator(a, b)
+	if (operator == '+') return add(a, b)
+	if (operator == '-') return sub(a, b)
+	if (operator == '*') return mult(a, b)
+	if (operator == '/') return div(a, b)
 }
+
+function clear() {
+	$('.display').html('0')
+}
+
+let pushed = $('.display').html()
+
+/*
+$('.num').click(e => {
+	$('.display').html($(e.target).html())
+})
+*/
+
+$('.num').click(e => {
+	$('.display').html($(e.target).html())
+})
+
+/*
+    var inputLabel = document.getElementById('inputLabel');
+     
+    function pushBtn(obj) {
+         
+        var pushed = obj.innerHTML;
+         
+        if (pushed == '=') {
+            // Calculate
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+             
+        } else if (pushed == 'AC') {
+            // All Clear
+            inputLabel.innerHTML = '0';
+             
+        } else {
+            if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+                 
+            } else {
+                inputLabel.innerHTML += pushed;
+                 
+            }
+        }
+    }
+
+*/
