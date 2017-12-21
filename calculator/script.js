601598fdbd6bd22e3e74aabf6b4815954254ccf3
Pablo <pablonm89@gmail.com>
2017-12-21 11:31:31 -0300

    Restart

--- calculator/script.js
+++ calculator/script.js
@@ -1,11 +1,3 @@
-//clears the equation
-$('.clear').click(_ => clear())
-
-const clear = () =>	{
-	aDisplay = []
-	$('.display').html('0')
-}
-
 const add = (a, b) => a + b
 const sub = (a, b) => a - b
 const mult = (a, b) => a * b
@@ -17,17 +9,4 @@ function operate(operator, a, b) {
 	if (operator == '*') return mult(a, b)
 	if (operator == '/') return div(a, b)
 }
-
-
-let aDisplay = []
-
-$('.num').click(e => {
-	aDisplay.push($(e.target).html())
-	$('.display').html(aDisplay)
-})
-
-$('.del').click(_ => {
-	aDisplay.pop()
-	$('.display').html(aDisplay)
-})
 
