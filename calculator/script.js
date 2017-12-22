a749c1fb73fa4debf74001c87bd67025c05afd3d
Pablo <pablonm89@gmail.com>
2017-12-22 12:46:57 -0300

    Calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,22 +1,66 @@
 let $display = $('.display')
+let $buttons = $('.btn')
 let tempDisplay = []
-let total = 0
-
-$('.num').click(e => {
-	tempDisplay.push($(e.target).html())
-	$display.html(tempDisplay)
-})
+let operator;
+let didOperate = false
+let a = ''
+let b = ''
 
 const add = (a,b) => a + b
 const sub = (a,b) => a - b
 const mul = (a,b) => a * b
 const div = (a,b) => a / b
 
-function operate(operator, a, b) {
+const operate = (ops, a, b) => {
 	a = parseFloat(a)
 	b = parseFloat(b)
-	if (operator == '+') return add(a,b)
-	if (operator == '-') return subs(a,b)
-	if (operator == '/') return mult(a,b)
-	if (operator == '*') return div(a,b)
+	if (ops == '+') return add(a,b)
+	if (ops == '-') return sub(a,b)
+	if (ops == '*') return mul(a,b)
+	if (ops == '/') return div(a,b)
 }
+
+function getInput(input) {
+	console.log(input)
+	if (input == 'AC') {
+		a = ''
+		b = ''
+		tempDisplay = []
+		$display.html('0')
+	} else if (input == 'back') {
+		tempDisplay.pop()
+		$display.html(tempDisplay)
+	} else if (/[0-9\.]/.test(input)) {
+		if (input == '.' && tempDisplay.join('').includes('.')) return
+		tempDisplay.push(input)
+		$display.html(tempDisplay.join(''))
+	} else if (/[+-/*]/.test(input)) {
+		if (a == '' || didOperate) {
+			a = tempDisplay.join('') || a
+			didOperate = false
+		} else {
+			b = tempDisplay.join('')
+				if (b) {
+					total = `${operate(operator, a, b)}`
+	        		a = total
+	        		tempDisplay = []
+					}
+			$display.html(a)
+		}
+		operator = input
+		tempDisplay = []
+	} else if (input == '=') {
+		b = tempDisplay.join('') || b
+		total = `${operate(operator, a, b)}`
+		if (total != "NaN" && total != "undefined") {
+     	a = total
+      	tempDisplay = []
+      	didOperate = true
+      	$display.html(a)
+    	}
+	}
+}
+
+$buttons.click(e => {
+	getInput($(e.target).html())
+})
